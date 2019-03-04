2dec8a84ab4082f9535d45c89d0e3b2badeee267
Lukas Neuberger <lukas@alttaler.de>
2019-03-04 14:28:06 +0100

    fixed CMake for zmq and simple zmq server

--- src/ECU_Communication/main.cpp
+++ src/ECU_Communication/main.cpp
@@ -1,11 +1,25 @@
 // A simple program that computes the square root of a number
 #include <stdio.h>
 #include <stdlib.h>
+#include <iostream>
 #include <math.h>
 #include "Config.h"
-//#include <zmq.h>
+#include <zmq.hpp>
 
 int main(int argc, char *argv[])
 {
+  zmq::context_t contect(1);
+  zmq::socket_t socket(contect, ZMQ_REP);
+  socket.bind("tcp://*:5555");
+  while (true)
+  {
+    zmq::message_t request;
+    socket.recv(&request);
+    std::cout << "Received Hello" << std::endl;
+
+    zmq::message_t reply(5);
+    memcpy(reply.data(), "World", 5);
+    socket.send(reply);
+  }
   return 0;
 }

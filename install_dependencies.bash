9e17b91088d7b979ece3176db364176f19add885
Lukas Neuberger <lukas@alttaler.de>
2019-03-04 20:08:39 +0100

    use amqpcpp and RabbitMQ

--- install_dependencies.bash
+++ install_dependencies.bash
@@ -1,6 +1,11 @@
 #!/bin/bash
 #installs all dependencies for this project
 
+#make sure system is up to date
+sudo apt-get update
+sudo apt-get upgrade -y
+
+#make directory for git repositories
 mkdir dependencies
 cd dependencies
 
@@ -20,4 +25,30 @@ mkdir build
 cd build
 cmake ..
 sudo make -j4 install
+cd ../..
+
+#OpenSplice
+git clone https://github.com/ADLINK-IST/opensplice.git
+cd opensplice
+
+sudo apt-get install -y gawk flex bison perl gsoap
+source ./configure armv6l.linux-dev
+echo "alias ospl=\"source $HOME/opensplice/install/HDE/$TARGET/release.com\"" >> ~/.bashrc
+source $HOME/.bashrc
+source $HOME/opensplice/install/HDE/$TARGET/release.com
+make
+make install
+cd ..
+
+#RabbitMQ
+sudo apt-get install rabbitmq-server -y
+
+#RabbitMQ C++
+git clone https://github.com/CopernicaMarketingSoftware/AMQP-CPP.git
+cd AMQP-CPP
+sudo apt-get install libssl-dev -y
+mkdir build
+cd build
+make
+make install
 cd ../..
--- src/ECU_Communication/main.cpp
+++ src/ECU_Communication/main.cpp
@@ -4,22 +4,9 @@
 #include <iostream>
 #include <math.h>
 #include "Config.h"
-#include <zmq.hpp>
+#include <amqpcpp.h>
 
 int main(int argc, char *argv[])
 {
-  zmq::context_t contect(1);
-  zmq::socket_t socket(contect, ZMQ_REP);
-  socket.bind("tcp://*:5555");
-  while (true)
-  {
-    zmq::message_t request;
-    socket.recv(&request);
-    std::cout << "Received Hello" << std::endl;
-
-    zmq::message_t reply(5);
-    memcpy(reply.data(), "World", 5);
-    socket.send(reply);
-  }
   return 0;
 }

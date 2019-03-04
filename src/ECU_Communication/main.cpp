7b1e9ca83d302e6cb474ad90ce79d5d398d3d71f
Lukas Neuberger <lukas@alttaler.de>
2019-03-04 11:52:57 +0100

    ECU_Communication init

---
+++ src/ECU_Communication/main.cpp
@@ -0,0 +1,11 @@
+// A simple program that computes the square root of a number
+#include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include "Config.h"
+//#include <zmq.h>
+
+int main(int argc, char *argv[])
+{
+  return 0;
+}

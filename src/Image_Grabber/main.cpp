87ed4ff72d0b9e830cd93c87f3b87fe6f8845cbd
Lukas Neuberger <git@alttaler.de>
2019-03-05 10:14:04 -0800

    simple subscriber and publisher using rabbitmq and mosquitto

--- src/Image_Grabber/main.cpp
+++ src/Image_Grabber/main.cpp
@@ -19,19 +19,7 @@ void handle_signal(int s)
 
 void connect_callback(struct mosquitto *mosq, void *obj, int result)
 {
-  printf("ECU_Communication connect callback, rc=%d\n", result);
-}
-
-void message_callback(struct mosquitto *mosq, void *obj, const struct mosquitto_message *message)
-{
-  bool match = 0;
-  printf("got message '%.*s' for topic '%s'\n", message->payloadlen, (char *)message->payload, message->topic);
-
-  mosquitto_topic_matches_sub("/devices/wb-adc/controls/+", message->topic, &match);
-  if (match)
-  {
-    printf("got message for ADC topic\n");
-  }
+  printf("Image_Grabber connect callback, rc=%d\n", result);
 }
 
 int main(int argc, char *argv[])
@@ -41,23 +29,23 @@ int main(int argc, char *argv[])
   int rc = 0;
 
   mosquitto_lib_init();
-  mosq = mosquitto_new("ECU_Communication", true, 0);
+  mosq = mosquitto_new("Image_Grabber", true, 0);
 
   if (mosq)
   {
     mosquitto_connect_callback_set(mosq, connect_callback);
-    mosquitto_message_callback_set(mosq, message_callback);
 
-    rc = mosquitto_connect(mosq, mqtt_host, mqtt_port, 60);
-
-    mosquitto_subscribe(mosq, NULL, "/test", 0);
+    rc = mosquitto_connect(mosq, mqtt_host, mqtt_port, 2);
 
     while (run)
     {
       rc = mosquitto_loop(mosq, -1, 1);
+      int mid;
+      mosquitto_publish(mosq, &mid, "/test", 5, "abcd", 0, false);
+      sleep(1);
       if (run && rc)
       {
-        printf("ECU_Communication connection error!\n");
+        printf("Image_Grabber connection error!\n");
         sleep(10);
         mosquitto_reconnect(mosq);
       }
